1d0f9d41c9e48fb9c8e58f8ea36b99961e4c769e
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-10-02 13:52:27 +0300

    Added a set method for the ringer.

--- src/android/ringerMode.java
+++ src/android/ringerMode.java
@@ -20,6 +20,9 @@ public class ringerMode extends CordovaPlugin {
         if (action.equals("getRingerMode")) {
             this.getRingerMode(callbackContext);
             return true;
+        } else if (action.equals("setRingerMode")) {
+            this.setRingerMode(args, callbackContext);
+            return true;
         }
         return false;
     }
@@ -39,5 +42,28 @@ public class ringerMode extends CordovaPlugin {
                 callbackContext.success("RINGER_MODE_NORMAL");
                 break;
         }
+        callbackContext.error("NONE");
+    }
+
+    private void setRingerMode(String strMode, CallbackContext callbackContext) {
+        Context context = this.cordova.getActivity().getApplicationContext();
+        AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+
+        int mode = Integer.parseInt(strMode); 
+        switch (mode) {
+            case 0:
+                audioManager.setRingerMode(RINGER_MODE_SILENT);
+                callbackContext.success("RINGER_MODE_SILENT");
+                break;
+            case 1:
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                callbackContext.success("RINGER_MODE_VIBRATE");
+                break;
+            case 2:
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL));
+                callbackContext.success("RINGER_MODE_NORMAL");
+                break;
+        }
+        callbackContext.error("NONE");
     }
 }
--- www/ringerMode.js
+++ www/ringerMode.js
@@ -4,4 +4,8 @@ ringerModeExport.getRingerMode = function(successCallback, errorCallback) {
   cordova.exec(successCallback, errorCallback, "ringerMode", "getRingerMode", []);
 };
 
+ringerModeExport.setRingerMode = function(mode, successCallback, errorCallback) {
+  cordova.exec(successCallback, errorCallback, "ringerMode", "setRingerMode", [mode]);
+};
+
 module.exports = ringerModeExport;
